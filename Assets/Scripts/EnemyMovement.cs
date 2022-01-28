e1922f2fad88dd0a1812e7df119b7cd354bf1fe0
bossie911 <62554007+bossie911@users.noreply.github.com>
2022-01-28 18:12:31 +0100

    attract enemy setup

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,8 +6,11 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     public NavMeshAgent agent;
+    public bool chasePlayer = false;
 
     private GameObject player;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +26,9 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (chasePlayer == true)
+        {
+            agent.destination = player.transform.position;
+        }
     }
 }
