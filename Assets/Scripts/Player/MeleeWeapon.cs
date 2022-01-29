4a2ea67a0cd544fa3757cb5832edfda0d8885b4f
DarrenDV <darrendevre@hotmail.com>
2022-01-29 19:46:51 +0100

    melee weapon deals damage

---
+++ Assets/Scripts/Player/MeleeWeapon.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : MonoBehaviour
+{
+
+    [SerializeField] private float damageToDeal = 50f;
+    public bool canDealDamage = true;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "MeleeEnemy")
+        {
+            if (canDealDamage)
+            {
+                other.GetComponent<EnemyHealth>().TakeDamage(damageToDeal);
+                canDealDamage = false;
+            }
+        }
+    }
+}
