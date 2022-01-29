4132af75152ed233789dd79fa99c0dcc2f5821c5
bossie911 <62554007+bossie911@users.noreply.github.com>
2022-01-29 22:43:53 +0100

    stop enemy walking and spawning when you die

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -28,6 +29,24 @@ public class PlayerHealth : MonoBehaviour
 
             // Death Animation
             _animator.SetTrigger("Death");
+            StopAllEnemies();   
+        }
+    }
+
+    void StopAllEnemies()
+    {
+        //List for active enemies
+        List<GameObject> activeEnemies = new List<GameObject>();
+        GameObject spawnParent = GameObject.FindWithTag("SpawnParent");
+        activeEnemies = spawnParent.GetComponent<EnemySpawning>().enemies;
+
+        //Stopping current spawning
+        spawnParent.GetComponent<EnemySpawning>().CancelInvoke();
+
+        //Stopping the movement for all active enemies
+        foreach (GameObject enemy in activeEnemies)
+        {
+            enemy.GetComponent<NavMeshAgent>().speed = 0f;
         }
     }
 }
