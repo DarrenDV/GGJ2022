6da31fa49b05798e1086aedef73f013bfe2ddbd8
bossie911 <62554007+bossie911@users.noreply.github.com>
2022-01-30 13:44:39 +0100

    random ranged enemy

--- Assets/Scripts/Enemies/EnemySpawning.cs
+++ Assets/Scripts/Enemies/EnemySpawning.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class EnemySpawning : MonoBehaviour
 {
     public GameObject meleeEnemyPrefab;
+    public GameObject rangedEnemyPrefab;
     public GameObject[] spawns;
     public List<GameObject> enemies = new List<GameObject>();
 
     public int totalEnemyPool = 100;
 
     private float spawnTime;
+    private int chanceForRanged = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -37,9 +39,18 @@ public class EnemySpawning : MonoBehaviour
         
         if (totalEnemyPool > 0) 
         {
+            int ran = Random.Range(0, chanceForRanged);
+            if (ran == 0)
+            {
+                GameObject enemy = Instantiate(rangedEnemyPrefab, spawns[spawnIndex].transform);
+                enemies.Add(enemy);
+            }
+            else
+            {
+                GameObject enemy = Instantiate(meleeEnemyPrefab, spawns[spawnIndex].transform);
+                enemies.Add(enemy);
+            }
             totalEnemyPool--;
-            GameObject enemy = Instantiate(meleeEnemyPrefab, spawns[spawnIndex].transform);
-            enemies.Add(enemy);
         }
     }
 }
